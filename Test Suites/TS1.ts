de5df5c1e803ec35b9641c5cefe7412c5ebb4cc2
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-11-02 10:52:28 +0900

    first commit

---
+++ Test Suites/TS1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-11-02T10:42:12</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>3a394db0-09c9-45e7-aa9c-50fd26cd0fc5</testSuiteGuid>
+   <testCaseLink>
+      <guid>00991def-5b71-4da4-8636-0361a7fe51b6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
